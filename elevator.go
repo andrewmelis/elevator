e11812c6bbcd66dc52d74f860aba790e454be08e
Andrew Melis <andrewmelis@gmail.com>
2017-01-07 18:29:38 -0600

    messy, but simple test passes

--- elevator.go
+++ elevator.go
@@ -2,6 +2,7 @@ package elevator
 
 import (
 	_ "fmt"
+	"math"
 )
 
 type direction string
@@ -11,25 +12,30 @@ const down direction = "down"
 
 type action string
 
-const open action = "open"
+const open action = "open" // turn these into types that implement command interface
 const close action = "close"
+const travelUp action = "travelUp"
 
 type Elevator struct {
 	Queue         []action
 	Floor         int
-	PassengerBays []map[*Passenger]bool
+	PassengerBays []map[*Passenger]bool // one waiting area per floor -- extract me
+	Riders        map[*Passenger]bool   // extract me. should be callback for riding passengers
 }
 
 func NewElevator(floors int) *Elevator {
 	var queue = make([]action, 0, 2)
-	var bays = make([]map[*Passenger]bool, floors) // accept # floors param?
+	var bays = make([]map[*Passenger]bool, floors)
 	for i := range bays {
 		bays[i] = make(map[*Passenger]bool)
 	}
+	var riders = make(map[*Passenger]bool)
+
 	return &Elevator{
 		Queue:         queue,
 		Floor:         0,
 		PassengerBays: bays,
+		Riders:        riders,
 	}
 }
 
@@ -44,11 +50,14 @@ func (e *Elevator) Tick() {
 		}
 		// end extract shift()
 
+		// make this ElevatorAction.perform()
 		switch current {
 		case open:
-			for p, _ := range e.PassengerBays[e.Floor] {
-				p.enter(e)
-			}
+			e.allowRidersToExit()
+			e.waitingPassengersEnter()
+		case travelUp:
+			e.Floor++
+
 		}
 	}
 }
@@ -56,10 +65,23 @@ func (e *Elevator) Tick() {
 func (e *Elevator) Call(p *Passenger, d direction) {
 	// TODO figure out how to add all necessary actions
 	e.Queue = append(e.Queue, open)
-	e.Queue = append(e.Queue, close)
-	//
+	e.enqueueDestination(p.CurrentFloor)
 
 	e.addWaitingPassenger(p)
+}
+
+func (e *Elevator) addRidingPassenger(p *Passenger) {
+	e.Riders[p] = true
+}
+
+func (e *Elevator) removeRidingPassenger(p *Passenger) {
+	delete(e.Riders, p)
+}
+
+func (e *Elevator) allowRidersToExit() {
+	for p := range e.Riders {
+		p.exit(e)
+	}
 }
 
 func (e *Elevator) addWaitingPassenger(p *Passenger) {
@@ -68,17 +90,41 @@ func (e *Elevator) addWaitingPassenger(p *Passenger) {
 
 func (e *Elevator) removeWaitingPassenger(p *Passenger) {
 	// handle error case?
-	bay := e.PassengerBays[p.CurrentFloor]
-	delete(bay, p)
+	delete(e.PassengerBays[p.CurrentFloor], p)
+}
+
+func (e *Elevator) waitingPassengersEnter() {
+	for p, _ := range e.PassengerBays[e.Floor] {
+		p.enter(e)
+	}
+}
+
+func (e *Elevator) enqueueDestination(destination int) {
+	// add necessary "travel" actions to go from currentFloor to input floor
+	// TODO start with one tick per floor
+	diff := math.Abs(float64(destination - e.Floor))
+
+	if diff > 0 {
+		e.Queue = append(e.Queue, close) // eww -- how handle open, then no button press?
+
+		for i := 0; i < int(diff); i++ {
+			e.Queue = append(e.Queue, travelUp)
+		}
+
+		// close? open?
+		// just hard code open for now
+		e.Queue = append(e.Queue, open)
+	}
 }
 
 type Passenger struct {
 	CurrentFloor int
+	Destination  int
 	InTransit    bool
 }
 
-func NewPassenger() *Passenger {
-	return &Passenger{}
+func NewPassenger(destination int) *Passenger {
+	return &Passenger{Destination: destination}
 }
 
 func (p *Passenger) Call(e *Elevator, d direction) {
@@ -87,5 +133,23 @@ func (p *Passenger) Call(e *Elevator, d direction) {
 
 func (p *Passenger) enter(e *Elevator) {
 	p.InTransit = true
-	e.removeWaitingPassenger(p)
+	e.removeWaitingPassenger(p) // elevator + bays should be separate
+	e.addRidingPassenger(p)
+	p.inputDestination(e)
+}
+
+func (p *Passenger) inputDestination(e *Elevator) {
+	e.enqueueDestination(p.Destination)
+}
+
+func (p *Passenger) exit(e *Elevator) {
+	if p.shouldExit(e) {
+		p.InTransit = false
+		p.CurrentFloor = e.Floor
+		e.removeRidingPassenger(p) // again, don't have elevator do bookkeeping
+	}
+}
+
+func (p *Passenger) shouldExit(e *Elevator) bool {
+	return p.Destination == e.Floor
 }
--- elevator_test.go
+++ elevator_test.go
@@ -5,15 +5,21 @@ import "fmt"
 
 const maxTicks = 10
 
+// CAVEAT: either tests must know how many 'ticks' a given set of actions takes,
+//         or the elevator needs to return a list of actions so we can check
+// TODO figure this out. consider testing DSL like Clean Code(ch 9)
+
 // Situation:
 // - single rider at floor 0 (ground).
 // - elevator idle, empty at floor 0
 // - rider going up to floor n; n > 0
 func TestElevatorOpensOnBottomFloor(t *testing.T) {
-	e := NewElevator(1)
-	p := NewPassenger()
+	nFloors := 2
+	topFloor := nFloors - 1
+	e := NewElevator(nFloors)
 
-	dest := 0 // CHANGE TO HIGHER NUMBER LATER
+	dest := topFloor // CHANGE TO HIGHER NUMBER LATER
+	p := NewPassenger(dest)
 	p.Call(e, up)
 
 	var foundPassengerInTransit bool
@@ -77,3 +83,15 @@ func TestElevatorOpensOnBottomFloor(t *testing.T) {
 // 		t.Errorf("passenger %v did not arrive at destination %d", p, dest)
 // 	}
 // }
+
+// Situation: Rider calls elevator then walks away
+
+// Situation: multiple riders -- serial, same bay, same dest
+
+// Situation: multiple riders -- same bay, same direction
+
+// Situation: multiple riders -- same bay, different directions
+
+// Situation: multiple riders -- different bays, different directions
+
+// all those ^^^, but don't start at level 0
